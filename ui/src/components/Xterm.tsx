18fa6c696226dc396b9ee44d750268697c93fa60
00o.sh <hello@00o.sh>
2025-02-13 09:37:46 -0600

    Update xterm dependency to @xterm/xterm and adjust imports accordingly

--- ui/src/components/Xterm.tsx
+++ ui/src/components/Xterm.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useLayoutEffect, useRef } from "react";
-import { Terminal } from "xterm";
+import { Terminal } from "@xterm/xterm";
 import { Unicode11Addon } from "@xterm/addon-unicode11";
 import { WebglAddon } from "@xterm/addon-webgl";
 import { WebLinksAddon } from "@xterm/addon-web-links";
 import { FitAddon } from "@xterm/addon-fit";
 import { ClipboardAddon } from "@xterm/addon-clipboard";
 
-import "xterm/css/xterm.css";
+import "@xterm/xterm/css/xterm.css";
 import { useRTCStore, useUiStore } from "../hooks/stores";
 
 const isWebGl2Supported = !!document.createElement("canvas").getContext("webgl2");
